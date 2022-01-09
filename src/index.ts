c8c8a54d9baec788c9e7f50b45ec29e7edaa41b4
jsoto <jonathan@visage.com>
2022-01-09 19:47:24 +0100

    more

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import noble from '@abandonware/noble'
 import { toMetricData } from './sensors/to-metric-data'
 import { CloudWatchClient, PutMetricDataCommand } from "@aws-sdk/client-cloudwatch"
 import credentials from './credentials'
-
+let i = 0
 // create cloud watch client
 const client = new CloudWatchClient({
     region:'us-east-1',
@@ -11,7 +11,7 @@ const client = new CloudWatchClient({
 
 const onStateChanged = (state) => {
     if (state === 'poweredOn') {
-        noble.startScanning([], true)
+        noble.startScanning([], false)
     } else {
         noble.stopScanning()
     }
@@ -20,6 +20,7 @@ const onStateChanged = (state) => {
 const onDiscovered = async (peripheral) => {
     const metricData = toMetricData(peripheral)
     if (metricData){
+        console.log(i++)
         const command = new PutMetricDataCommand(metricData)
         await client.send(command)
     }  
